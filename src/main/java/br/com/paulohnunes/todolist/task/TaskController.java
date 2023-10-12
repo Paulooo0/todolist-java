211468b57a8d7c8aedd26c59fd548c3260ad2714
Paulooo0 <paulohenrique.n.s00@gmail.com>
2023-10-12 13:03:57 -0300

    ADD: filter ADD: task UPDATE: pom.xml UPDATE: UserController.java

---
+++ src/main/java/br/com/paulohnunes/todolist/task/TaskController.java
@@ -0,0 +1,43 @@
+package br.com.paulohnunes.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID) userId);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de inicio/termino deve ser maior do que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de inicio/termino deve ser maior do que a data atual");
+        }
+
+        
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+}
--- src/main/java/br/com/paulohnunes/todolist/user/UserController.java
+++ src/main/java/br/com/paulohnunes/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,11 @@ public class UserController {
             System.out.println("Usuario ja existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
         }
+
+        var hashedPassword = BCrypt.withDefaults().
+        hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashedPassword);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
