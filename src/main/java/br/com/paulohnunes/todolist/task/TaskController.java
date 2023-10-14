a1871b811a16768db229f1aab2725a0f8a0beb69
Paulooo0 <paulohenrique.n.s00@gmail.com>
2023-10-14 10:30:42 -0300

    ADD: Utils copyNonNullProperties and getNullPropertyNames
    UPDATE: ITaskRepository
    added findByUserId
    
    UPDATE: TaskController
    ID identification and update tasks from same ID
    
    FIX: FilterTaskAuth
    changed  to  to the  route

--- src/main/java/br/com/paulohnunes/todolist/task/TaskController.java
+++ src/main/java/br/com/paulohnunes/todolist/task/TaskController.java
@@ -1,16 +1,22 @@
 package br.com.paulohnunes.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.paulohnunes.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -36,8 +42,24 @@ public class TaskController {
                 .body("A data de inicio/termino deve ser maior do que a data atual");
         }
 
-        
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
